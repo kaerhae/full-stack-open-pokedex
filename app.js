bc51f18e5cff8131a5b49da6e875999636800ecd
kaerhae <erno.karha@hotmail.com>
2025-09-11 22:18:58 +0300

    Simulate broken deployment; fix lint

--- app.js
+++ app.js
@@ -7,9 +7,9 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
-app.get('/health', (req, res) => {
+app.get('/health', (_, _) => {
   // eslint-disable-next-line no-constant-condition
-  if(true) throw('Error')
+  if(true) throw Error('Simulating failed deployment')
   //res.status(200).send('ok')
 })
 
