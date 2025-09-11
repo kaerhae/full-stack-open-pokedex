a9065bc88b0892c196af49cf76167b1a5659e567
kaerhae <erno.karha@hotmail.com>
2025-09-11 22:14:59 +0300

    Simulate broken deployment

--- app.js
+++ app.js
@@ -8,7 +8,9 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/health', (req, res) => {
-  res.status(200).send('ok')
+  // eslint-disable-next-line no-constant-condition
+  if(true) throw('Error')
+  //res.status(200).send('ok')
 })
 
 app.listen(PORT, () => {
